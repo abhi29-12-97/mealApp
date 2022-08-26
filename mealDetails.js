da995be6cf59902781b09ceb0fea370aeca015e3
Abhishek garg <abhigarg7099@gmail.com>
2022-08-26 19:18:32 +0530

    all functionality added

--- mealDetails.js
+++ mealDetails.js
@@ -1,4 +1,11 @@
-const ele = [];
+let ele = [];
 loader = () => {
   ele = JSON.parse(sessionStorage.getItem("meal"));
+  console.log(ele);
+  let mealname = document.querySelector("[meal-name]");
+  let mealimage = document.querySelector("[meal-image]");
+  let mealdesc = document.querySelector("[meal-desc]");
+  mealname.textContent = ele[0].strMeal;
+  mealimage.src = ele[0].strMealThumb;
+  mealdesc.textContent = ele[0].strInstructions;
 };
